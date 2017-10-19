305b897a8d6d462fe670e4764ab61076f23af675
Vikas <potlurivikas@gmail.com>
2017-10-19 14:29:30 -0500

    Add internal linking and UI improvements

--- assets/js/init.js
+++ assets/js/init.js
@@ -1,12 +1,38 @@
+$("body").removeClass("no-js");
+window.smoothScroll = function (input) {
+
+	let location = input;
+
+	if(typeof input.preventDefault === "function") {
+		input.preventDefault();
+		console.log('Event Callback [smoothScroll] called');
+		location = $(this).attr("href");
+	}
+
+	if(location === "#" || $(location).length) {
+		let scrollTop = (location == "#") ? 0 :
+			($(location).offset().top - $("header").height() + 1);
+		$("html, body").animate({scrollTop});
+		window.location.hash = location;
+		return true;
+	} else {
+		console.warn('Unable to scroll to', location, 'because it does not exist');
+		return false;
+	}
+};
+
 $(document).ready(function () {
+
+	const hash = window.location.hash;
+
+	if(hash.indexOf('#') >= 0) {
+		console.log('Hash exists - attempting to update position');
+		if(window.smoothScroll(hash))
+			console.log("...success");
+	}
+
     $('.button-collapse').sideNav();
     $('.parallax').parallax();
 
-    $(".scroller").click(function (event) {
-    	event.preventDefault();
-
-    	$("html,body").animate({
-    		scrollTop: $("#secondary").offset().top - $("header").height()
-    	}, 1000);
-   	})
-})
+    $(".smoothscroll, .smooth-scroll").click(window.smoothScroll);
+});
